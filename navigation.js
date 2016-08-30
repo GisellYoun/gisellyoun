a1ed39304a259c0d46bda5289c70147ede7c77bc
GisellYoun <GisellYoun@yahoo.com>
2016-08-29 20:25:10 -0700

    Add files via upload

--- navigation.js
+++ navigation.js
@@ -5,4 +5,5 @@ $('.tab').click(function(){ $('.tabs-wrap').hide(); //Hide all tab content
 
 
 $(function(){ $(".nav li a").click(function(e){ e.preventDefault(); //To prevent the default anchor tag behaviour 
+																 console.log("Loading " + url);
 		var url = this.href; $(".main").load(url); }); });
